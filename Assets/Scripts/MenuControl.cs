91f18dbef1b5123b9618db431506d7512e579b1f
mustafa.ay <aymustafa.32@gmail.com>
2021-02-20 17:54:16 +0300

    SpaceAdventure

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] musicIcons;
+
+    [SerializeField]
+    Button musicButton;
+
+    bool musicOn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void HighScore()
+    {
+        SceneManager.LoadScene("Score");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void Music()
+    {
+        if (musicOn)
+        {
+            musicOn = false;
+            musicButton.image.sprite = musicIcons[0];
+        }
+        else
+        {
+            musicOn = true;
+            musicButton.image.sprite = musicIcons[1];
+        }
+    }
+}
